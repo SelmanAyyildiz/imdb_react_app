310931a262fa48c5d84bd6e8384e37a3905186fb
SelmanAyyildiz <ayyildizslmn@gmail.com>
2021-07-11 00:14:20 +0200

    add Link react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router,Route , Switch } from 'react-router-dom'
-
+import Fixbar from './component/navbar/fixbar'
 import Movie from './Movie'
 import MovieDetail from './MovieDetail'
 
@@ -8,6 +8,7 @@ import MovieDetail from './MovieDetail'
 function App() {
     return (
        <Router>
+           <Fixbar/>
        <Switch>
            <Route path = "/detail/:id" component={MovieDetail} exact/>
            <Route path = "/" component={Movie}/>
--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -2,6 +2,7 @@
 import "./style.css";
 import {useRef, useContext} from "react";
 import { movieContext } from "../../Movie";
+import Fixbar from "./fixbar";
 
 export const Navbar = () => {
     const searchInputRef = useRef();
@@ -17,6 +18,7 @@ export const Navbar = () => {
         <div className="navbar-wrapper">
             <input  ref={searchInputRef} type="text" onKeyDown={handleKey}/>
             <button onClick={()=>{setSearchKey(searchInputRef?.current?.value)}}>search</button>
+            <Fixbar/>
         </div>
     )
 }
